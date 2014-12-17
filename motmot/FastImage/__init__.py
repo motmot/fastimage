1abdb7d83003efd8a8759e82931ed47215b1da02
Andrew Straw <strawman@astraw.com>
2014-12-17 16:22:52 +0100

    bump version for release

---
+++ motmot/FastImage/__init__.py
@@ -0,0 +1,1 @@
+__version__='0.5.6' # also in setup.py
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name="motmot.FastImage",
       description="Pythonic API for the framewave SIMD library",
       url='http://code.astraw.com/projects/motmot',
       license="BSD",
-      version='0.5.5',
+      version='0.5.6', # also in motmot/FastImage/__init__.py
       namespace_packages=['motmot'],
       packages = find_packages(),
       ext_modules=cythonize([Extension(name="motmot.FastImage.FastImage",
