acef83da5087257a376af411ee3cc3f02e6bf06f
Andrew Straw <strawman@astraw.com>
2014-12-17 16:08:45 +0100

    convert release of GIL to Cython idiom

--- src/FastImage.pyx
+++ src/FastImage.pyx
@@ -257,12 +257,11 @@ cdef class FastImageBase:
         if not c_python.PyFile_Check(fobj):
             raise ValueError('need file object')
         fd = c_python.PyFile_AsFile(fobj)
-        c_python.Py_BEGIN_ALLOW_THREADS
-        for i from 0 <= i < self.imsiz.sz.height:
-            rowptr = self.im+i*self.step
-            nbytes = c_lib.fwrite( rowptr, 1, self.imsiz.sz.width*bytes_per_pixel, fd )
-            nbytes_tot = nbytes_tot+nbytes
-        c_python.Py_END_ALLOW_THREADS
+        with nogil:
+            for i from 0 <= i < self.imsiz.sz.height:
+                rowptr = self.im+i*self.step
+                nbytes = c_lib.fwrite( rowptr, 1, self.imsiz.sz.width*bytes_per_pixel, fd )
+                nbytes_tot = nbytes_tot+nbytes
         return nbytes_tot
 
     property _step:
@@ -611,11 +610,11 @@ cdef class FastImage8u(FastImageBase):
 
         sz.width = size.sz.width
         sz.height = size.sz.height
-        c_python.Py_BEGIN_ALLOW_THREADS # release GIL
-        sts = fic.ficiMinIndx_8u_C1R(
-            <fic.Fic8u*>self.im,self.step,
-            sz, &min_val, &index_x, &index_y)
-        c_python.Py_END_ALLOW_THREADS # acquire GIL
+        with nogil:
+            c_python.Py_BEGIN_ALLOW_THREADS # release GIL
+            sts = fic.ficiMinIndx_8u_C1R(
+                <fic.Fic8u*>self.im,self.step,
+                sz, &min_val, &index_x, &index_y)
         CHK_FIC_HAVEGIL(sts)
         return min_val, index_x, index_y
 
@@ -627,11 +626,10 @@ cdef class FastImage8u(FastImageBase):
 
         sz.width = size.sz.width
         sz.height = size.sz.height
-        c_python.Py_BEGIN_ALLOW_THREADS # release GIL
-        sts = fic.ficiMaxIndx_8u_C1R(
-            <fic.Fic8u*>self.im,self.step,
-            sz, &max_val, &index_x, &index_y)
-        c_python.Py_END_ALLOW_THREADS # acquire GIL
+        with nogil:
+            sts = fic.ficiMaxIndx_8u_C1R(
+                <fic.Fic8u*>self.im,self.step,
+                 sz, &max_val, &index_x, &index_y)
         CHK_FIC_HAVEGIL(sts)
         return max_val, index_x, index_y
 
@@ -929,11 +927,10 @@ cdef class FastImage32f(FastImageBase):
 
         sz.width = size.sz.width
         sz.height = size.sz.height
-        c_python.Py_BEGIN_ALLOW_THREADS # release GIL
-        sts = fic.ficiMaxIndx_32f_C1R(
-            <fic.Fic32f*>self.im,self.step,
-            sz, &max_val, &index_x, &index_y)
-        c_python.Py_END_ALLOW_THREADS # acquire GIL
+        with nogil:
+            sts = fic.ficiMaxIndx_32f_C1R(
+                <fic.Fic32f*>self.im,self.step,
+                 sz, &max_val, &index_x, &index_y)
         CHK_FIC_HAVEGIL(sts)
         return max_val, index_x, index_y
 
