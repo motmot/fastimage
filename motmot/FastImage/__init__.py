75a8e3cf20c3c9a1d8443f7f633d1d915caadbce
Andrew Straw <strawman@astraw.com>
2016-11-01 13:51:37 +0100

    bump version number

--- motmot/FastImage/__init__.py
+++ motmot/FastImage/__init__.py
@@ -1,1 +1,1 @@
-__version__='0.5.6.ipp' # also in setup.py
+__version__='0.6.0' # also in setup.py
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name="motmot.FastImage",
       "Integrated Performance Primitives (Intel IPP) library",
       url='http://code.astraw.com/projects/motmot',
       license="BSD",
-      version='0.5.6.ipp', # also in motmot/FastImage/__init__.py
+      version='0.6.0', # also in motmot/FastImage/__init__.py
       namespace_packages=['motmot'],
       packages = find_packages(),
       ext_modules=cythonize([Extension(name="motmot.FastImage.FastImage",
