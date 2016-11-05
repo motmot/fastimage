3df4e566afb3fc6f1c43b64adcf07e6711d9ae9c
Andrew Straw <strawman@astraw.com>
2016-11-05 20:43:34 +0100

    build with IPP 2017

--- motmot/FastImage/util.py
+++ motmot/FastImage/util.py
@@ -35,28 +35,10 @@ system_install - (Linux only) True if IPP installed in /usr, False if in /opt
 
     vals = {}
 
-    # static/shared ideas from IPP 5.2 ipp-samples/data-compression/Makefile.osx
-    # See also http://support.intel.com/support/performancetools/libraries/ipp/sb/CS-021491.htm
-    if sys.platform.startswith('linux'):
-        libdirname = 'lib'
-        incdirname = 'include'
-        if system_install:
-            IPPROOT = '/usr'
-        else:
-            IPPROOT = '/opt/intel/ipp/%s/%s'%(ipp_version,ipp_arch,)
-    elif sys.platform == 'darwin':
-        libdirname = 'Libraries'
-        incdirname = 'Headers'
-        IPPROOT='/Library/Frameworks/Intel_IPP.framework/Versions/%s/%s'%(ipp_version,ipp_arch)
-    elif sys.platform.startswith('win'):
-        if ipp_static:
-            libdirname = 'lib'
-        else:
-            libdirname = 'stublib'
-        incdirname = 'include'
-        IPPROOT=r'C:\Program Files\Intel\IPP\%s\%s'%(ipp_version,ipp_arch)
-    else:
-        raise NotImplementedError("no build support implemented for this platform")
+    IPPROOT = os.environ['IPPROOT']
+
+    libdirname = 'lib'
+    incdirname = 'include'
     ipp_define_macros = []
     ipp_extra_link_args = []
     if sys.platform.startswith('win'):
@@ -73,37 +55,11 @@ system_install - (Linux only) True if IPP installed in /usr, False if in /opt
         LIB_ARCH=ipp_arch
     ARPOSTFIX='a'
     LIBPREFIX='lib'
-    if ipp_static:
-        ipp_libraries_merged = [libname+'merged%s'%(LIB_ARCH,) for libname in ipp_libraries]
-        ipp_libraries_emerged = [libname+'emerged%s'%(LIB_ARCH,) for libname in ipp_libraries]
-        ipp_libraries = ipp_libraries_emerged + ipp_libraries_merged
-        if sys.platform.startswith('win'):
-            ipp_libraries.append('ippcorel')
-        else:
-            ipp_extra_link_args.append(
-                os.path.join( IPPROOT,libdirname,LIBPREFIX+'ippcore'+LIB_ARCH+'.'+ARPOSTFIX))
-        ipp_define_macros.append(('FASTIMAGE_STATICIPP',None))
-        if sys.platform.startswith("linux") and machine == 'x86_64':
-            # See http://softwarecommunity.intel.com/isn/Community/en-US/forums/thread/30233343.aspx
-            ipp_extra_link_args.append('--pic-executable')
-    else:
-        ipp_libraries.append('ippcore')
-        ipp_libraries = [libname+'%s'%(LIB_ARCH,) for libname in ipp_libraries]
-        if not sys.platform.startswith('win'):
-            ipp_libraries.append('guide')
+    ipp_libraries.append('ippcore')
     vals['extra_link_args'] = ipp_extra_link_args
     vals['ipp_library_dirs'] = ipp_library_dirs
     vals['ipp_libraries'] = ipp_libraries
     vals['ipp_define_macros'] = ipp_define_macros
     vals['ipp_include_dirs'] = ipp_include_dirs
-    if sys.platform == 'darwin' and not ipp_static:
-        print '*'*80
-        print '*'*80
-        print 'WARNING: due to brain-dead Intel IPP library issues, you must do the following:'
-        print
-        print 'export DYLD_LIBRARY_PATH="/Library/Frameworks/Intel_IPP.framework/Versions/5.2/%s/Libraries"'%(ipp_arch,)
-        print
-        print '*'*80
-        print '*'*80
 
     return vals
